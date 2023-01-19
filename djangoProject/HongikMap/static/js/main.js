2c65576d8c7edc13b7e7014b731dd52c1f9ef154
yooooonshine <108210104+yooooonshine@users.noreply.github.com>
2023-01-19 21:37:02 +0900

    Update main.js

--- djangoProject/HongikMap/static/js/main.js
+++ djangoProject/HongikMap/static/js/main.js
@@ -1,5 +1,15 @@
-startKeyword = document.getElementById("startKeyword");
+const bookBtn = document.querySelector('.bookmark-btt');
+const section = document.querySelector('section');
+const label = document.querySelector('label');
 
-if (startKeyword.length > 2){
-    console.log("js");
-}
+// 이부분이 발생시키기
+bookBtn.addEventListener('click', function(){
+	section.style.left = 0;
+	label.style.opacity = 0;
+})
+
+//이부분이 없애기
+bookBtn.addEventListener('blur',function(){
+	section.style.left = '-200px';
+	label.style.opacity = 1;
+})
