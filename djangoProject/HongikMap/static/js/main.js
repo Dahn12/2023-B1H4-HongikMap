c29b8f41b1bbfa20922322f757edf77d27d14b2c
yooooonshine <108210104+yooooonshine@users.noreply.github.com>
2023-01-20 13:43:16 +0900

    Update main.js

--- djangoProject/HongikMap/static/js/main.js
+++ djangoProject/HongikMap/static/js/main.js
@@ -1,15 +1,13 @@
-const bookBtn = document.querySelector('.bookmark-btt');
+const bookBtn = document.querySelector('.getDirection-btt');
 const section = document.querySelector('section');
-const label = document.querySelector('label');
 
-// 이부분이 발생시키기
 bookBtn.addEventListener('click', function(){
 	section.style.left = 0;
-	label.style.opacity = 0;
+
 })
 
-//이부분이 없애기
-bookBtn.addEventListener('blur',function(){
-	section.style.left = '-200px';
-	label.style.opacity = 1;
+
+bookBtn.addEventListener('click',function(){
+	section.style.left = '-350px';
+
 })
