5a6cc47f43975be13f6b973cd67c963084811b64
이승훈 <qwqw8019@naver.com>
2023-01-20 16:55:56 +0900

    chore: features.py, views.py 수정

--- djangoProject/HongikMap/features.py
+++ djangoProject/HongikMap/features.py
@@ -1,26 +1,26 @@
 class Recommend:
     def __init__(self):
-        self.keywords = dict()
         self.recommends = dict()
+        self.keywords = dict()
 
         with open("static/data/keywords.txt", "r") as kw:
             for line in kw.readlines():
                 entity, value = line.split(":")
-                self.keywords[entity] = list(value.split(","))
+                self.recommends[entity] = list(value.split(","))
 
         with open('static/data/recommends.txt', "r") as rec:
             for line in rec.readlines():
                 entity, value = line.split(":")
-                self.recommends[entity] = value
+                self.keywords[entity] = value
 
-        print(self.keywords)
-        print(self.recommends)
+        print(self.recommends, self.keywords)
+
 
     def find(self, keyword: str):
         ret = []
-        for k, v in self.keywords.items():
+        for k, v in self.recommends.items():
             if any([keyword in x for x in v]):
-                ret.append((k, self.recommends[k]))
+                ret.append((k, self.keywords[k]))
 
         if not ret:
             if keyword[0].isalpha() and keyword[0] != "Z":
--- djangoProject/HongikMap/views.py
+++ djangoProject/HongikMap/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from . import features
 
 
@@ -10,3 +11,9 @@ def welcome(request):
 
 def main(request):
     return render(request, 'HongikMap/main.html', {})
+
+
+def recommend(request):
+    start_input = request.POST.get('start_input')
+    end_input = request.POST.get()
+    return JsonResponse({})
