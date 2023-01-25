2e71685db41604ea8d3ce78a155473928e6d6163
이승훈 <qwqw8019@naver.com>
2023-01-25 17:32:19 +0900

    feat: 서버 시작 시 dijkstra 연산 및 파일 생성

--- djangoProject/HongikMap/apps.py
+++ djangoProject/HongikMap/apps.py
@@ -1,6 +1,26 @@
 from django.apps import AppConfig
+from . import features
 
 
 class HongikmapConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'HongikMap'
+
+    graph_with_elevator = features.Graph()
+    graph_without_elevator = features.Graph(elevator=False)
+
+    path_with_elevator = features.Path(graph_with_elevator)
+    path_without_elevator = features.Path(graph_without_elevator)
+
+    for start in graph_with_elevator.rooms:
+        path_with_elevator.dijkstra(start)
+
+    with open("HongikMap/static/data/result_with_elevator.txt", "w") as f:
+        for key, value in path_with_elevator.result.items():
+            f.write(f'{key} {value}')
+
+    for start in graph_without_elevator.rooms:
+        path_without_elevator.dijkstra(start)
+    with open("HongikMap/static/data/result_without_elevator.txt", "w") as f:
+        for key, value in path_without_elevator.result.items():
+            f.write(f'{key} {value}')
--- djangoProject/HongikMap/features.py
+++ djangoProject/HongikMap/features.py
@@ -1,3 +1,6 @@
+import heapq
+
+
 class Recommend:
     def __init__(self):
         self.recommends = dict()
@@ -13,7 +16,7 @@ class Recommend:
                 entity, value = line.split(":")
                 self.keywords[entity] = value
 
-        #print(self.recommends, self.keywords)
+        # print(self.recommends, self.keywords)
 
     def find(self, keyword: str):
 
@@ -48,7 +51,97 @@ class Recommend:
 
 
 class Graph:
-    def __init__(self, elevator:bool = True):
+    def __init__(self, elevator: bool = True):
+        self.rooms = []
+        self.nodes = []
+        self.weights = []
+        self.useless = []
         with open("HongikMap/static/data/data.txt", "r") as f:
             for line in f.readlines():
-                print(line)
+                equality = False
+                line_length = len(line.split())
+                if "#" in line or line_length not in [3, 4]:
+                    self.useless.append(line)
+                    continue
+
+                if line_length == 4 and line.split()[3] == "t":
+                    equality = True
+                start, end, weight = line.split()[:3]
+                weight = int(weight)
+
+                if self.check_invalidity(start, line) or self.check_invalidity(end, line):
+                    continue
+
+                self.verify_and_insert(start)
+                self.verify_and_insert(end)
+
+                if (start, end, weight) not in self.weights:
+                    self.weights.append((start, end, weight))
+                if equality:
+                    if (end, start, weight) not in self.weights:
+                        self.weights.append((end, start, weight))
+
+    def check_invalidity(self, node: str, line):
+        if len(node.split("-")) != 3:
+            self.useless.append(line)
+            return True
+        return False
+
+    def verify_and_insert(self, node):
+        building, floor, entity = node.split("-")
+
+        if node not in self.nodes:
+            self.nodes.append(node)
+
+        if node not in self.rooms and entity.isdecimal():
+            self.rooms.append(node)
+
+
+class Path:
+    def __init__(self, graph: Graph):
+        self.nodes = graph.nodes
+        self.weights = graph.weights
+        self.rooms = graph.rooms
+        self.result = dict()
+
+    def dijkstra(self, start: str):
+        path = {key: {'distance': 1e9 if key != start else 0, 'parent': key} for key in self.nodes}
+
+        graph = {key: [] for key in self.nodes}
+        for key in graph:
+            # for k, w in self.weights.items():
+            #     if k[0] == key:
+            #         graph[key].append((k[1], w))
+
+            # for (_, adj), weight in filter(lambda x: x[0][0] == key, self.weights.items()):
+            #     graph[key].append((adj, weight))
+
+            for _, adj, weight in filter(lambda x: x[0] == key, self.weights):
+                graph[key].append((adj, weight))
+
+        q = []
+        heapq.heappush(q, (0, start))
+
+        while q:
+            dist, now = heapq.heappop(q)
+            if path[now]['distance'] < dist:
+                continue
+
+            for adj, weight in graph[now]:
+                cost = dist + weight
+                if cost < path[adj]['distance']:
+                    path[adj]['distance'] = cost
+                    path[adj]['parent'] = now
+                    heapq.heappush(q, (cost, adj))
+
+        self.store(start, path)
+
+    def store(self, start: str, path: dict):
+        for end in self.rooms:
+            route = [end]
+            while route[-1] != start:
+                route.append(path[route[-1]]['parent'])
+            self.result[(start, end)] = {'distance': path[end]['distance'], 'route': route[::-1]}
+
+    def find(self, start, end):
+        return self.result[(start, end)]
