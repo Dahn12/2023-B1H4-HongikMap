85fd2faeb3914e737582e5e7a60be39d48161567
leesh <qwqw8019@naver.com>
2023-02-20 16:59:53 +0900

    feat: Mysql Database 설정
    settings.py
    database 부분 변경

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -73,8 +73,12 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hongikmap',
+        'USER': 'root',
+        'PASSWORD': 'B1H4HongikMap!@#$',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
