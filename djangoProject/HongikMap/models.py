ae6db4af0337acfacd6e7f1068a96a0b27538af1
leesh <qwqw8019@naver.com>
2023-02-20 17:04:17 +0900

    feat: 모델 함수 정의
    models.py

--- djangoProject/HongikMap/models.py
+++ djangoProject/HongikMap/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
+
 # Create your models here.
+def initialize_database():
+    pass
+
+
+def initialize_table():
+    pass
+
+
+def initialize_node_table():
+    pass
+
+
+def initialize_result_with_elevator_table():
+    pass
+
+
+def initialize_result_without_elevator_table():
+    pass
+
+
+def initialize_coordinate_table():
+    pass
+
+
+def save_building(building: str, result: dict):
+    pass
+
+
+def get_route(node: str) -> dict:
+    pass
+
+
+def get_coordinate(node: str) -> (int, int):
+    pass
