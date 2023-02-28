462423fcc26712fc68e23c86a1c1248734b9e5c6
yooooonshine <hl3470@naver.com>
2023-03-01 00:15:48 +0900

    Feat: 관리자페이지 update페이지 기능 구현

---
+++ djangoProject/HongikMap/static/js/update.js
@@ -0,0 +1,11 @@
+function preprocessingSubmit(e) {
+    if (e.target.id == "buildingButton") {
+        txtName = $('#buildingName').val() + '.txt'
+
+
+    } else if (e.target.id == "XtoXButton") {
+
+    } else if (e.target.id == "externalNodeButton") {
+
+    }
+}
