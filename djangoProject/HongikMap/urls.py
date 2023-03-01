03ec9738625e743c41953b43839f6b7cb0dc841d
darkenergy814 <79552567+darkenergy814@users.noreply.github.com>
2023-03-01 14:51:41 +0900

    fix: delete main2

--- djangoProject/HongikMap/urls.py
+++ djangoProject/HongikMap/urls.py
@@ -5,7 +5,6 @@ urlpatterns = [
     path('', views.welcome, name='welcome'),
     # path('main', views.main, name='main'),
     path('main', views.responsiveWebMain, name='main'),
-    path('main2', views.main2, name='main2'),
     path('recommend', views.recommend, name='recommend'),
     path('place_submit', views.submit, name='submit'),
     path('preprocessing', views.preprocessing, name='preprocessing'),
