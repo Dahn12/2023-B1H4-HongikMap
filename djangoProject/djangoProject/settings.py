bc5b502b8eea25f9aaab9194dba42f14362ad6e6
leesh <qwqw8019@naver.com>
2023-03-03 19:10:59 +0900

    chore: sqlite3 적용

--- djangoProject/djangoProject/settings.py
+++ djangoProject/djangoProject/settings.py
@@ -73,15 +73,10 @@ WSGI_APPLICATION = 'djangoProject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'hongikmap',
-        'USER': 'root',
-        'PASSWORD': 'B1H4HongikMap!@#$',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
